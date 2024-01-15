01e74d6b7985e062ff5e6b620bdb756d3106f702
adityjha0 <aditya2005aps@gmail.com>
2024-01-15 11:42:44 +0530

    q2

--- asgn1a/q2/program.s
+++ asgn1a/q2/program.s
@@ -3,7 +3,8 @@ prompt:
 	.ascii "Enter 10 integers separated by <Enter>.\n"
 array:
 	.space 40
-	
+s:
+	.ascii "The sum is : "
 .global _start
 
 .text
@@ -16,13 +17,29 @@ _start:
 
 	li t0, 0
 	li t1, 10
+	li t2, 0
 	
 takeint:
 	bge t0, t1, sum
-		
 	
+	li a7, 5
+	ecall
+	
+	add t2, t2, a0
+	addi t0, t0, 1
+	j takeint
 	
 sum:
+	li a7, 64
+	li a0, 1
+	la a1, s
+	li a2, 13
+	ecall
+	
+	li a7, 1
+	mv a0, t2
+	ecall
+	
 	#exit with exit-call 0
 	li a7, 93
 	li a0, 0
