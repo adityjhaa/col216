978791c63882534d1e7c1ffd0d9b66e1c7586ebe
adityjha0 <aditya2005aps@gmail.com>
2024-01-15 11:30:41 +0530

    correct test case

--- asgn1a/q2/program.s
+++ asgn1a/q2/program.s
@@ -1,9 +1,28 @@
+.data
+prompt:
+	.ascii "Enter 10 integers separated by <Enter>.\n"
+array:
+	.space 40
+	
 .global _start
 
 .text
 _start:
+	li a7, 64
+	li a0, 1
+	la a1, prompt
+	li a2, 40
+	ecall
 
-
+	li t0, 0
+	li t1, 10
+	
+takeint:
+	bge t0, t1, sum
+		
+	
+	
+sum:
 	#exit with exit-call 0
 	li a7, 93
 	li a0, 0
