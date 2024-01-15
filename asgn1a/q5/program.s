a4e0c21b946edffcd6c9818a792dc204e55f3957
adityjha0 <aditya2005aps@gmail.com>
2024-01-15 22:01:48 +0530

    q5

--- asgn1a/q5/program.s
+++ asgn1a/q5/program.s
@@ -1,6 +1,6 @@
 .data
 prompt:
-	.ascii "Enter an integer\n"
+	.ascii "Enter an integer n : \n"
 buffer:	.space 4
 	
 .global _start
@@ -10,22 +10,38 @@ _start:
 	li a7, 64
 	li a0, 1
 	la a1, prompt
-	li a2, 17
+	li a2, 22
 	ecall
 	
-	li a7, 5
+	li a7, 5 #read int
 	ecall
 	
 	li t0, 0
 	li t1, 1
-	mv t2, a0
+	mv t2, a0 #entered integer n
+	li t3, 0
+	li t4, 1
 	
+	ble t2, t1, base #for base case n<=1, where fib(n) = n
 fibonacci:
-	beqz t2, z
-	beq t2
+	bge t4, t2, rec
+	add t3, t0, t1 #fib(n) = fib(n-2) + fib(n-1)
+	mv t0, t1
+	mv t1, t3
+	addi t4, t4, 1
+	j fibonacci
 				
-			
+base:
+	li a7, 1
+	ecall	
+	j exit
+rec:
+	li a7, 1
+	mv a0, t3
+	ecall
+	j exit
 exit:
+	#exit with exit-call 0
 	li a7, 93
 	li a0, 0
 	ecall
