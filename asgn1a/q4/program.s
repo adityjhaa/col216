9f2c60b0745d950be8be7fd300f1748c7ea03946
adityjha0 <aditya2005aps@gmail.com>
2024-01-18 23:58:36 +0530

    basic structure

--- asgn1a/q4/program.s
+++ asgn1a/q4/program.s
@@ -2,9 +2,17 @@
 
 .text
 _start:
-
+	li t5, 0
+	li t6, 0
+first:
+	li t0, 10
+	li a7, 12
+	ecall
+	beq a0, t0, second
+	addi a0, a0, -48
+	addi 
+	addi t5, t5, 1
 	
-	
-	li a7, 93
-	li a0, 0
+exit:	#2022CS11102	
+	li a7, 10 #exit with code 0
 	ecall
