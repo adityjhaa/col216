21ba57617188e75e6d27b8f33ce09fc6098ba876
adityjha0 <aditya2005aps@gmail.com>
2024-01-25 19:20:21 +0530

    q1 solved

--- asgn1b/q1/program.s
+++ asgn1b/q1/program.s
@@ -6,30 +6,53 @@
 _start:
 	li a7, 5
 	ecall
+	mv t6, a0
 	
-	mv t0, a0
-	li t6, 1
-	li t5, 1
-	li t4, 0
+	li a7, 5
+	ecall
+	mv t5, a0
+	
+	sub t4, t6, t5
+	bltz t4, zeroa
+	
+	li t0, 1
+	mv t1, t6
 	jal a2, factorial
+	mv s0, t0
 	
+	li t0, 1
+	mv t1, t5
+	jal a2, factorial
+	mv s1, t0	
 	
-	mv a0, t5
+	li t0, 1
+	mv t1, t4
+	jal a2, factorial
+	mv s2, t0
+
+	div s0, s0, s1
+	div s0, s0, s2
+
 	li a7, 1
+	mv a0, s0
 	ecall
 	
-exit:
+exit: #2022CS11102
 	li a7, 10
 	ecall
-	
+
+
 factorial:
-	beqz t0, return
-	bgt t6, t0, return
-	
-	mul t5, t5, t6
-	
-	addi t6, t6, 1
+	blez t1, return
+	mul t0, t0, t1
+	addi t1, t1, -1
 	j factorial
 	
 return:
-	jr a2
+	jr a2	
+
+zeroa:
+	li a0, 0
+	li a7, 1
+	ecall
+	j exit
