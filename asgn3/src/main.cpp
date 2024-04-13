01d52f7d0a30fc5660f183f0c28ed03fab38f057
adityjha0 <aditya2005aps@gmail.com>
2024-04-13 07:40:54 +0530

    basic inputs for program

--- asgn3/src/main.cpp
+++ asgn3/src/main.cpp
@@ -1,4 +1,30 @@
-int main(){
+#include <iostream>
+
+// Required values
+int sets,
+    blocks,
+    bytes,
+    write_allocate,
+    write_through,
+    lru;
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 7)
+    {
+        std::cout << argc << "\n";
+        puts("Invalid input arguments");
+
+        return 1;
+    }
+
+    sets = atoi(argv[1]);
+    blocks = atoi(argv[2]);
+    bytes = atoi(argv[3]);
+
+    ((std::string)(argv[4]) == "write-allocate") ? write_allocate = 1 : write_allocate = 0;
+    ((std::string)argv[5] == "write-through") ? write_through = 1 : write_through = 0;
+    ((std::string)argv[6] == "lru") ? lru = 1 : lru = 0;
 
     return 0;
 }
