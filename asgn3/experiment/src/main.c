168cca59459a1f68e543e518a15bf2378c2fa846
adityjha0 <aditya2005aps@gmail.com>
2024-04-20 14:56:55 +0530

    init and free

--- asgn3/experiment/src/main.c
+++ asgn3/experiment/src/main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int **initialize(int n)
+{
+	int **a;
+
+	a = calloc(n, sizeof(int *));
+
+	srand(time(NULL));
+
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = calloc(n, sizeof(int));
+		for (int j = 0; j < n; j++)
+		{
+			a[i][j] = rand() % 1000;
+		}
+	}
+
+	return a;
+}
+
+void freemat(int **mat, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		free(mat[i]);
+	}
+	free(mat);
+}
 
 int main()
 {
@@ -8,13 +38,13 @@ int main()
 	printf("0 : Matrix Multiplication\n");
 	printf("1 : Transpose Matrix\n?- ");
 
-	if(scanf("%d", &a) != 1)
+	if (scanf("%d", &a) != 1)
 	{
 		printf("Error reading input.\n");
 		return 1;
 	}
-	
-	switch(a)
+
+	switch (a)
 	{
 	case 0:
 		// Matrix Multiplication
@@ -22,12 +52,24 @@ int main()
 		break;
 	case 1:
 		// Transpose Matrix
-	
+
 		break;
 	default:
 		printf("Number not 0/1\n");
 	}
-	
+
+	int **mat = initialize(5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+
+	freemat(mat, 5);
+
 	return 0;
 }
-
