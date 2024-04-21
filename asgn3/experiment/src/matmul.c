ef4a9f9407859a7f80cce452bfdef79dd0ca2d47
adityjha0 <aditya2005aps@gmail.com>
2024-04-21 13:04:48 +0530

    added all ordered loops

--- asgn3/experiment/src/matmul.c
+++ asgn3/experiment/src/matmul.c
@@ -13,3 +13,102 @@ void matmul(int **a, int **b, int **c, int n)
 		}
 	}
 }
+
+// different ordered loops
+
+// 1
+
+void matmul1(int **a, int **b, int **c, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+// 2
+
+void matmul2(int **a, int **b, int **c, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+// 3
+
+void matmul3(int **a, int **b, int **c, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+// 4
+
+void matmul4(int **a, int **b, int **c, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+// 5
+
+void matmul5(int **a, int **b, int **c, int n)
+{
+	for (int k = 0; k < n; k++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+// 6
+
+void matmul6(int **a, int **b, int **c, int n)
+{
+	for (int k = 0; k < n; k++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
--- asgn3/experiment/src/matmul.h
+++ asgn3/experiment/src/matmul.h
@@ -3,4 +3,18 @@
 
 void matmul(int **a, int **b, int **c, int n);
 
+// different ordered loops
+
+void matmul1(int **a, int **b, int **c, int n);
+
+void matmul2(int **a, int **b, int **c, int n);
+
+void matmul3(int **a, int **b, int **c, int n);
+
+void matmul4(int **a, int **b, int **c, int n);
+
+void matmul5(int **a, int **b, int **c, int n);
+
+void matmul6(int **a, int **b, int **c, int n);
+
 #endif
