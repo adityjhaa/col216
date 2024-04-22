4caf14c947984cb8273ab5cf95365c7043e7f857
adityjha0 <aditya2005aps@gmail.com>
2024-04-22 19:31:50 +0530

    added orderings to transpose

--- asgn3/experiment/src/transpose.c
+++ asgn3/experiment/src/transpose.c
@@ -7,6 +7,10 @@ void swap(int *a, int *b)
     *b = t;
 }
 
+/* Standard functions */
+
+// in-place
+
 void transpose1(int **mat, int n)
 {
     for (int i = 0; i < n; i++)
@@ -18,11 +22,62 @@ void transpose1(int **mat, int n)
     }
 }
 
+// using an additional matrix
+
 void transpose2(int **mat1, int **mat2, int n)
 {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
+        {
+            mat2[i][j] = mat1[j][i];
+        }
+    }
+}
+
+/* Orderings */
+
+// in-place
+void transpose1a(int **mat, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(&mat[i][j], &mat[j][i]);
+        }
+    }
+}
+
+void transpose1b(int **mat, int n)
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = j + 1; i < n; i++)
+        {
+            swap(&mat[i][j], &mat[j][i]);
+        }
+    }
+}
+
+// additional matrix
+
+void transpose2a(int **mat1, int **mat2, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mat2[i][j] = mat1[j][i];
+        }
+    }
+}
+
+void transpose2b(int **mat1, int **mat2, int n)
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
         {
             mat2[i][j] = mat1[j][i];
         }
